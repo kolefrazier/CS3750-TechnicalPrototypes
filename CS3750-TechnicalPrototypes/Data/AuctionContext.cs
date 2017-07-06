e14a4392280bcf2c47cedb449f2e65e4b94e30d7
Kole <kolefrazier@mail.weber.edu>
2017-07-06 14:16:36 -0600

    Removed hyphen from solution namespace, added Data and VM folders.

---
+++ CS3750-TechnicalPrototypes/Data/AuctionContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS3750-TechnicalPrototypes.Data
+{
+    public class AuctionContext
+    {
+    }
+}
--- CS3750-TechnicalPrototypes/Startup.cs
+++ CS3750-TechnicalPrototypes/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace CS3750_TechnicalPrototypes
+namespace CS3750TechnicalPrototypes
 {
     public class Startup
     {
