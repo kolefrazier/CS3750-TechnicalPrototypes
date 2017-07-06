36d39a4ed1de905eef756ba47cf26733f6f92be1
Kole <kolefrazier@mail.weber.edu>
2017-07-06 14:20:33 -0600

    Fixed some namespace names in views.

--- CS3750-TechnicalPrototypes/Data/AuctionContext.cs
+++ CS3750-TechnicalPrototypes/Data/AuctionContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CS3750-TechnicalPrototypes.Data
+namespace CS3750TechnicalPrototypes.Data
 {
     public class AuctionContext
     {
