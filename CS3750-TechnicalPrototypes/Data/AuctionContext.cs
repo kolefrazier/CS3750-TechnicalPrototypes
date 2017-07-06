4a67c42e8a6dc60ba92cb7b324d673280e9172bf
Kole <kolefrazier@mail.weber.edu>
2017-07-06 14:42:18 -0600

    EntityFramework setup

--- CS3750-TechnicalPrototypes/Data/AuctionContext.cs
+++ CS3750-TechnicalPrototypes/Data/AuctionContext.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CS3750TechnicalPrototypes.Models;
 
 namespace CS3750TechnicalPrototypes.Data
 {
-    public class AuctionContext
-    {
-    }
+	public class AuctionContext : DbContext
+	{
+		public AuctionContext(DbContextOptions<AuctionContext> options) : base(options)
+		{
+
+		}
+
+		//Any database models need to be listed here as DbSet<type> properties.
+		//	Be sure to add them to the method below, OnModelCreating().
+		public DbSet<Auction> Auctions { get; set; }
+
+		/// <summary>
+		/// Removes plurality from DbSet property names when creating the table.
+		/// </summary>
+		/// <param name="modelBuilder"></param>
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Auction>().ToTable("Auction");
+		}
+	}
 }
---
+++ CS3750-TechnicalPrototypes/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS3750TechnicalPrototypes.Models;
+
+namespace CS3750TechnicalPrototypes.Data
+{
+	public class DbInitializer
+	{
+		public static void Initialize(AuctionContext context)
+		{
+			context.Database.EnsureCreated();
+
+			//Check for existing data. If found, exit method, else seed database.
+			if (context.Auctions.Any())
+				return;
+
+			//Seed data
+			//	No need to specify primary keys. Foreign keys need to be set up with LINQ.
+			//	Tip: Use arrays with object-initializers. Fill out as much data as you can.
+			//	Syntax: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/how-to-initialize-objects-by-using-an-object-initializer#example-1
+			//	Tip: With "one-to-many" relations, start with the "one" then do the "many". (A game has many reviews. Games initalized then reviews initialized.)
+
+			var auctions = new Auction[]
+			{
+				new Auction { AuctionName = "One auction, ha ha ha" },
+				new Auction { AuctionName = "Two auction, ha ha ha" }
+			};
+
+			foreach (Auction a in auctions)
+			{
+				context.Auctions.Add(a);
+			}
+		}
+	}
+}
