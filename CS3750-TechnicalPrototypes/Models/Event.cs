c9a5fd4b9ad8dc2c8278afbf26a6abf0a42bc18a
xarla_000 <xarla_000@GAMING-PC-GARY>
2017-07-11 01:04:23 -0600

    I added the event model

---
+++ CS3750-TechnicalPrototypes/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS3750TechnicalPrototypes.Models
+{
+    public class Event
+    {
+            [Key]
+            public int EventID { get; set; }
+            public int AuctionID { get; set; }
+            public string eventTitle { get; set; }
+            public DateTime openingEventDate { get; set; }
+            public DateTime closingEventDate { get; set; }
+            public string eventDescription { get; set; }
+    }
+}
