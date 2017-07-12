cbcaee1096799c842959926b0a3336d171e1e319
Kole <kolefrazier@mail.weber.edu>
2017-07-12 12:00:30 -0600

    Added a (logically borked) "All Bids For Auction" page.

---
+++ CS3750-TechnicalPrototypes/Models/ViewModels/BidDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS3750TechnicalPrototypes.Models;
+
+namespace CS3750TechnicalPrototypes.Models.ViewModels
+{
+    public class BidDetails
+    {
+		public BidHistory BidHistory { get; set; }
+		public Bidder Bidder { get; set; }
+		public Item Item { get; set; }
+	}
+}
